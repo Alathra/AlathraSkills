61cbc379d883e6b0cfcd3369ca85f93706105717
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-02-16 12:40:11 +0100

    feat: added gui helper class

---
+++ src/main/java/io/github/Alathra/AlathraSkills/gui/GuiHelper.java
@@ -0,0 +1,39 @@
+package io.github.Alathra.AlathraSkills.gui;
+
+import com.github.milkdrinkers.colorparser.ColorParser;
+import dev.triumphteam.gui.guis.Gui;
+
+public class GuiHelper {
+
+    public enum GuiType {
+        MAIN,
+        SKILL
+    }
+
+    public static Gui buildGui(GuiType type) {
+        Gui gui;
+        switch (type) {
+            case MAIN -> {
+                gui = Gui.gui()
+                    .rows(6)
+                    .title(ColorParser.of("<dark_grey>[<green>AlathraSkills<dark_grey>]").build()) // TODO: config
+                    .disableItemDrop()
+                    .disableItemPlace()
+                    .disableItemSwap()
+                    .disableItemTake()
+                    .create();
+            }
+            case SKILL -> {
+                gui = Gui.gui()
+                    .rows(3)
+                    .title(ColorParser.of("<dark_grey>[<green>AlathraSkills<dark_grey>]").build()) // TODO: config
+                    .disableItemDrop()
+                    .disableItemPlace()
+                    .disableItemSwap()
+                    .disableItemTake()
+                    .create();
+            }
+        }
+    }
+
+}
