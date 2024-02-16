6ef6193b73689c7b06eacf65f427ceb327ec69c7
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-02-16 12:51:21 +0100

    feat: added skill populate classes

---
+++ src/main/java/io/github/Alathra/AlathraSkills/gui/skill/PopulateButtons.java
@@ -0,0 +1,11 @@
+package io.github.Alathra.AlathraSkills.gui.skill;
+
+import dev.triumphteam.gui.guis.Gui;
+
+public class PopulateButtons {
+
+    public static void populateButtons (Gui gui) {
+        // TODO: Add buttons
+    }
+
+}
---
+++ src/main/java/io/github/Alathra/AlathraSkills/gui/skill/PopulateContent.java
@@ -0,0 +1,11 @@
+package io.github.Alathra.AlathraSkills.gui.skill;
+
+import dev.triumphteam.gui.guis.Gui;
+
+public class PopulateContent {
+
+    public static void populateContent(Gui gui) {
+        // TODO: Add content
+    }
+
+}
