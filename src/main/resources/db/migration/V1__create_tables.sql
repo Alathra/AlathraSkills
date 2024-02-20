578473ab868ae7acf8e8c13959b8c38d0ea8bcf9
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-02-20 15:24:47 +0100

    fix(db): corrected sql create queries

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -2,14 +2,12 @@ CREATE TABLE IF NOT EXISTS ${tablePrefix}player_skillcategoryinfo (
     uuid ${uuidType} NOT NULL,
     skillcategoryid INT NOT NULL,
     exp FLOAT NOT NULL,
-    PRIMARY KEY (uuid, skillcategoryid),
-    FOREIGN KEY (skillcategoryid)
+    PRIMARY KEY (uuid, skillcategoryid)
 )${tableDefaults};
 
 
 CREATE TABLE IF NOT EXISTS ${tablePrefix}player_skillinfo (
     uuid ${uuidType} NOT NULL,
     skillid INT NOT NULL,
-    PRIMARY KEY (uuid, skillid),
-    FOREIGN KEY (skillid)
+    PRIMARY KEY (uuid, skillid)
 )${tableDefaults};
