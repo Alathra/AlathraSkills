9d9f1165bb037a355a7d9572e9914b6bca947f69
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2024-03-06 16:43:42 -0500

    fix: Delete src/main/java/io/github/Alathra directory

--- src/main/java/io/github/Alathra/AlathraSkills/db/flyway/DatabaseMigrationException.java
+++
@@ -1,7 +0,0 @@
-package io.github.alathra.alathraskills.db.flyway;
-
-public class DatabaseMigrationException extends Exception {
-    public DatabaseMigrationException(Throwable t) {
-        super(t);
-    }
-}
