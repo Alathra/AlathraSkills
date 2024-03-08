151d89b3357ad83bf3c84b884946b7d1616bc329
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-03-08 22:12:18 +0100

    feat: added save the trees skill

---
+++ src/main/java/io/github/alathra/alathraskills/skills/woodcutting/SaveTheTreesSkill.java
@@ -0,0 +1,29 @@
+package io.github.alathra.alathraskills.skills.woodcutting;
+
+import com.github.milkdrinkers.colorparser.ColorParser;
+import io.github.alathra.alathraskills.AlathraSkills;
+import io.github.alathra.alathraskills.api.SkillsManager;
+import io.github.alathra.alathraskills.skills.Skill;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Collections;
+
+public class SaveTheTreesSkill extends Skill {
+    private SkillsManager skillsManager;
+
+    public SaveTheTreesSkill(int id) {
+        super(id, "Save the trees", "Save the trees and your time with automatic replanting!");
+
+        ItemStack icon = new ItemStack(Material.OAK_SAPLING);
+        ItemMeta meta = icon.getItemMeta();
+        meta.displayName(ColorParser.of("<green><bold>" + super.getName() + "</green></bold>").build());
+        meta.lore(Collections.singletonList(ColorParser.of("<gray><italics>" + super.getDescription() + "</gray></italics>").build()));
+        icon.setItemMeta(meta);
+        super.setIcon(icon);
+
+        skillsManager = AlathraSkills.getSkillsManager();
+        super.setCategory(skillsManager.skillCategories.get(3));
+    }
+}
