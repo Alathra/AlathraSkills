73cb4b297e0852dbd5f861f7c738bbeeebf0c753
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2024-03-09 19:39:57 -0500

    need: customblockdata dependency

---
+++ src/main/java/io/github/alathra/alathraskills/hooks/CustomBlockDataHook.java
@@ -0,0 +1,33 @@
+package io.github.alathra.alathraskills.hooks;
+
+import io.github.alathra.alathraskills.AlathraSkills;
+import io.github.alathra.alathraskills.Reloadable;
+
+public class CustomBlockDataHook implements Reloadable {
+	
+	private final AlathraSkills plugin;
+	
+	public CustomBlockDataHook(AlathraSkills plugin) {
+        this.plugin = plugin;
+    }
+	
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		CustomBlockData.registerListener(plugin);
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/io/github/alathra/alathraskills/utility/PDCUtil.java
@@ -0,0 +1,11 @@
+package io.github.alathra.alathraskills.utility;
+
+public class PDCUtil {
+	/**
+	 * Utility class that implements CustomBlockData API. This class is used to
+	 * handle Persistent Data Containers (PDCs). PDC is used to store data in blocks
+	 * to determine if blocks at a given location are natural or not.
+	 */
+	
+	
+}
