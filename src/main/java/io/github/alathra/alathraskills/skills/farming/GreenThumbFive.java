60a44794144b4235b8c0df5966bc0ba54ee93453
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2024-04-27 13:12:27 -0400

    feat: Green Thumb skills

--- src/main/java/io/github/alathra/alathraskills/skills/farming/GreenThumbFive.java
+++ src/main/java/io/github/alathra/alathraskills/skills/farming/GreenThumbFive.java
@@ -4,30 +4,27 @@ import com.github.milkdrinkers.colorparser.ColorParser;
 import io.github.alathra.alathraskills.AlathraSkills;
 import io.github.alathra.alathraskills.api.SkillsManager;
 import io.github.alathra.alathraskills.skills.Skill;
-import io.github.alathra.alathraskills.skills.farming.util.FarmingBlockUtil;
 import io.github.alathra.alathraskills.skills.farming.util.FarmingData;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.Ageable;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
-public class GreenThumb extends Skill {
+public class GreenThumbFive extends Skill {
 
-    private static double GREEN_THUMB_ONE_CHANCE = 0.10;
+    private static double GREEN_THUMB_FIVE_CHANCE = 0.60;
     private SkillsManager skillsManager;
 
-    public GreenThumb(int id, String name, String description) {
-        super(id, "Green Thumb 1", "Get a chance to bone meal crops in a wider area!");
+    public GreenThumbFive(int id, String name, String description) {
+        super(id, "Green Thumb 5", "Get a chance to bone meal crops in a wider area!");
 
         ItemStack icon = new ItemStack(Material.IRON_HOE);
         ItemMeta meta = icon.getItemMeta();
         meta.displayName(ColorParser.of("<green><bold>" + super.getName() + "</green></bold>").build());
-        meta.lore(List.of(ColorParser.of("<yellow>Level 1</yellow>").build(),
+        meta.lore(List.of(ColorParser.of("<yellow>Level 5</yellow>").build(),
             ColorParser.of("<red><italics>" + super.getDescription() + "</gray></italics>").build()));
         icon.setItemMeta(meta);
         super.setIcon(icon);
@@ -38,7 +35,7 @@ public class GreenThumb extends Skill {
 
     // Called on BlockFertilizeEvent
     public static void runFastHarvestOne(Block initialCrop) {
-        if (Math.random() >= GREEN_THUMB_ONE_CHANCE)
+        if (Math.random() >= GREEN_THUMB_FIVE_CHANCE)
             return;
 
         if (FarmingData.getBonemealableCrops().contains(initialCrop.getRelative(BlockFace.NORTH).getType())) {
@@ -56,6 +53,23 @@ public class GreenThumb extends Skill {
         if (FarmingData.getBonemealableCrops().contains(initialCrop.getRelative(BlockFace.WEST).getType())) {
             initialCrop.applyBoneMeal(BlockFace.WEST);
         }
+
+        if (FarmingData.getBonemealableCrops().contains(initialCrop.getRelative(BlockFace.NORTH_EAST).getType())) {
+            initialCrop.applyBoneMeal(BlockFace.NORTH_EAST);
+        }
+
+        if (FarmingData.getBonemealableCrops().contains(initialCrop.getRelative(BlockFace.NORTH_WEST).getType())) {
+            initialCrop.applyBoneMeal(BlockFace.NORTH_WEST);
+        }
+
+        if (FarmingData.getBonemealableCrops().contains(initialCrop.getRelative(BlockFace.SOUTH_EAST).getType())) {
+            initialCrop.applyBoneMeal(BlockFace.SOUTH_EAST);
+        }
+
+        if (FarmingData.getBonemealableCrops().contains(initialCrop.getRelative(BlockFace.SOUTH_WEST).getType())) {
+            initialCrop.applyBoneMeal(BlockFace.SOUTH_WEST);
+        }
+
 
     }
 }
--- src/main/java/io/github/alathra/alathraskills/skills/farming/GreenThumbTwo.java
+++ src/main/java/io/github/alathra/alathraskills/skills/farming/GreenThumbTwo.java
@@ -4,30 +4,27 @@ import com.github.milkdrinkers.colorparser.ColorParser;
 import io.github.alathra.alathraskills.AlathraSkills;
 import io.github.alathra.alathraskills.api.SkillsManager;
 import io.github.alathra.alathraskills.skills.Skill;
-import io.github.alathra.alathraskills.skills.farming.util.FarmingBlockUtil;
 import io.github.alathra.alathraskills.skills.farming.util.FarmingData;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.data.Ageable;
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
-public class GreenThumb extends Skill {
+public class GreenThumbTwo extends Skill {
 
-    private static double GREEN_THUMB_ONE_CHANCE = 0.10;
+    private static double GREEN_THUMB_TWO_CHANCE = 0.50;
     private SkillsManager skillsManager;
 
-    public GreenThumb(int id, String name, String description) {
-        super(id, "Green Thumb 1", "Get a chance to bone meal crops in a wider area!");
+    public GreenThumbTwo(int id, String name, String description) {
+        super(id, "Green Thumb 2", "Get a chance to bone meal crops in a wider area!");
 
         ItemStack icon = new ItemStack(Material.IRON_HOE);
         ItemMeta meta = icon.getItemMeta();
         meta.displayName(ColorParser.of("<green><bold>" + super.getName() + "</green></bold>").build());
-        meta.lore(List.of(ColorParser.of("<yellow>Level 1</yellow>").build(),
+        meta.lore(List.of(ColorParser.of("<yellow>Level 2</yellow>").build(),
             ColorParser.of("<red><italics>" + super.getDescription() + "</gray></italics>").build()));
         icon.setItemMeta(meta);
         super.setIcon(icon);
@@ -38,7 +35,7 @@ public class GreenThumb extends Skill {
 
     // Called on BlockFertilizeEvent
     public static void runFastHarvestOne(Block initialCrop) {
-        if (Math.random() >= GREEN_THUMB_ONE_CHANCE)
+        if (Math.random() >= GREEN_THUMB_TWO_CHANCE)
             return;
 
         if (FarmingData.getBonemealableCrops().contains(initialCrop.getRelative(BlockFace.NORTH).getType())) {
