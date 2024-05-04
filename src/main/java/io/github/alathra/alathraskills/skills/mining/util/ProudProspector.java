b839845f9ae4247170b2266eed96748387157018
ShermansWorld <77093412+ShermansWorld@users.noreply.github.com>
2024-05-04 13:08:29 -0400

    feat: Proud Prospector completed

--- src/main/java/io/github/alathra/alathraskills/skills/mining/util/ProudProspector.java
+++ src/main/java/io/github/alathra/alathraskills/skills/mining/util/ProudProspector.java
@@ -1,7 +1,9 @@
 package io.github.alathra.alathraskills.skills.mining.util;
 
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class ProudProspector {
 
@@ -9,6 +11,30 @@ public class ProudProspector {
     public static void run(BlockBreakEvent event, int skillLevel) {
         if (Math.random() >= getChance(skillLevel))
             return;
+
+        if (event.isCancelled()) {
+            return;
+        }
+
+        // If there are no drops, return
+        if (event.getBlock().getDrops().isEmpty()) {
+            return;
+        }
+
+        // If tool is silk touch, drop an ore block
+        ItemStack tool = event.getPlayer().getInventory().getItemInMainHand();
+        if (tool.hasItemMeta()) {
+            if (tool.getItemMeta().getEnchants().containsKey(Enchantment.SILK_TOUCH)) {
+                event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(event.getBlock().getType()));
+                return;
+            }
+        }
+
+        // If no silk touch, duplicate drops
+        for (ItemStack itemStack : event.getBlock().getDrops()) {
+            // Find the drops of the block and drop them again, effectively doubling the drops
+            event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), itemStack);
+        }
 
     }
 
