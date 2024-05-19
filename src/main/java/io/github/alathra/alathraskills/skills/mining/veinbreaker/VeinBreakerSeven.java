cf5722f945edaee9d25328d760a8099264a5a901
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-05-19 23:23:23 +0200

    fix: registered all skills
    and added the missing vein breakers

--- src/main/java/io/github/alathra/alathraskills/skills/mining/veinbreaker/VeinBreakerSeven.java
+++ src/main/java/io/github/alathra/alathraskills/skills/mining/veinbreaker/VeinBreakerSeven.java
@@ -10,17 +10,17 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
-public class VeinBreakerOne extends Skill {
+public class VeinBreakerSeven extends Skill {
 
     private static SkillsManager skillsManager;
 
-    public VeinBreakerOne(int id) {
-        super(id, "Vein Breaker 1", "5% chance to break all adjacent ores of the same kind.");
+    public VeinBreakerSeven(int id) {
+        super(id, "Vein Breaker 7", "5% chance to break all adjacent ores of the same kind.");
 
         ItemStack icon = new ItemStack(Material.DIAMOND_PICKAXE);
         ItemMeta meta = icon.getItemMeta();
         meta.displayName(ColorParser.of("<cyan><bold>" + super.getName() + "</cyan>").build());
-        meta.lore(List.of(ColorParser.of("<yellow>Level 2</yellow>").build(),
+        meta.lore(List.of(ColorParser.of("<yellow>Level 18</yellow>").build(),
             ColorParser.of("<orange>" + super.getDescription() + "</orange>").build()));
         icon.setItemMeta(meta);
         super.setIcon(icon);
