a811179d7dabe619579c2aebc13be7843f44bcd2
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-06-12 22:08:52 +0200

    refactor: moved sql create query to V1 file

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -11,3 +11,10 @@ CREATE TABLE IF NOT EXISTS ${tablePrefix}player_skillinfo (
     skillid INT NOT NULL,
     PRIMARY KEY (uuid, skillid)
 )${tableDefaults};
+
+CREATE TABLE IF NOT EXISTS ${tablePrefix}player_used_skill_points (
+    uuid ${uuidType} NOT NULL,
+    skills_used INT NOT NULL,
+    PRIMARY KEY (uuid)
+)${tableDefaults};
+
--- src/main/resources/db/migration/V2__create_used_skill_points_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS ${tablePrefix}player_used_skill_points (
-    uuid ${uuidType} NOT NULL,
-    skills_used INT NOT NULL,
-    PRIMARY KEY (uuid)
-)${tableDefaults};
