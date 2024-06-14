3235fa37f4f6085e9868acbe930afa6005c4177b
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-06-14 21:49:23 +0200

    fix: added wood to woodcutting exp listener

--- src/main/java/io/github/alathra/alathraskills/listeners/exp/WoodcuttingExpListener.java
+++ src/main/java/io/github/alathra/alathraskills/listeners/exp/WoodcuttingExpListener.java
@@ -37,14 +37,14 @@ public class WoodcuttingExpListener implements Listener {
         }
 
         float expAmount = switch (block.getType()) {
-            case OAK_LOG, STRIPPED_OAK_LOG -> Cfg.get().getFloat("experience.woodcutting.oak");
-            case SPRUCE_LOG, STRIPPED_SPRUCE_LOG -> Cfg.get().getFloat("experience.woodcutting.spruce");
-            case BIRCH_LOG, STRIPPED_BIRCH_LOG -> Cfg.get().getFloat("experience.woodcutting.birch");
-            case JUNGLE_LOG, STRIPPED_JUNGLE_LOG -> Cfg.get().getFloat("experience.woodcutting.jungle");
-            case ACACIA_LOG, STRIPPED_ACACIA_LOG -> Cfg.get().getFloat("experience.woodcutting.acacia");
-            case DARK_OAK_LOG, STRIPPED_DARK_OAK_LOG -> Cfg.get().getFloat("experience.woodcutting.darkOak");
-            case MANGROVE_LOG, STRIPPED_MANGROVE_LOG -> Cfg.get().getFloat("experience.woodcutting.mangrove");
-            case CHERRY_LOG, STRIPPED_CHERRY_LOG -> Cfg.get().getFloat("experience.woodcutting.cherry");
+            case OAK_LOG, STRIPPED_OAK_LOG, OAK_WOOD, STRIPPED_OAK_WOOD -> Cfg.get().getFloat("experience.woodcutting.oak");
+            case SPRUCE_LOG, STRIPPED_SPRUCE_LOG, SPRUCE_WOOD, STRIPPED_SPRUCE_WOOD -> Cfg.get().getFloat("experience.woodcutting.spruce");
+            case BIRCH_LOG, STRIPPED_BIRCH_LOG, BIRCH_WOOD, STRIPPED_BIRCH_WOOD -> Cfg.get().getFloat("experience.woodcutting.birch");
+            case JUNGLE_LOG, STRIPPED_JUNGLE_LOG, JUNGLE_WOOD, STRIPPED_JUNGLE_WOOD -> Cfg.get().getFloat("experience.woodcutting.jungle");
+            case ACACIA_LOG, STRIPPED_ACACIA_LOG, ACACIA_WOOD, STRIPPED_ACACIA_WOOD -> Cfg.get().getFloat("experience.woodcutting.acacia");
+            case DARK_OAK_LOG, STRIPPED_DARK_OAK_LOG, DARK_OAK_WOOD, STRIPPED_DARK_OAK_WOOD -> Cfg.get().getFloat("experience.woodcutting.darkOak");
+            case MANGROVE_LOG, STRIPPED_MANGROVE_LOG, MANGROVE_WOOD, STRIPPED_MANGROVE_WOOD -> Cfg.get().getFloat("experience.woodcutting.mangrove");
+            case CHERRY_LOG, STRIPPED_CHERRY_LOG, CHERRY_WOOD, STRIPPED_CHERRY_WOOD -> Cfg.get().getFloat("experience.woodcutting.cherry");
             case CRIMSON_STEM, STRIPPED_CRIMSON_STEM -> Cfg.get().getFloat("experience.woodcutting.crimson");
             case WARPED_STEM, STRIPPED_WARPED_STEM -> Cfg.get().getFloat("experience.woodcutting.warped");
             default -> 0.0f;
