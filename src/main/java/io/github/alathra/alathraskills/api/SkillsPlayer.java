b9a37d1e365cf5a68cabe2d906f7b5f717c8927c
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-06-15 14:57:33 +0200

    fix: null check skillsplayer cooldown

--- src/main/java/io/github/alathra/alathraskills/api/SkillsPlayer.java
+++ src/main/java/io/github/alathra/alathraskills/api/SkillsPlayer.java
@@ -161,6 +161,7 @@ public class SkillsPlayer {
     }
 
     public boolean isOnCooldown() {
+        if (this.cooldown == null) return false;
         return this.cooldown.isAfter(Instant.now());
     }
 
