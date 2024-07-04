e0553b73d776fee5c1eb09a69ff9898bbd6af1fa
rooooose-b <142353909+rooooose-b@users.noreply.github.com>
2024-07-04 02:13:18 +0200

    refactor: supply instance and refactored switch statement

--- src/main/java/io/github/alathra/alathraskills/skills/mining/util/EasyPicking.java
+++ src/main/java/io/github/alathra/alathraskills/skills/mining/util/EasyPicking.java
@@ -1,6 +1,7 @@
 package io.github.alathra.alathraskills.skills.mining.util;
 
 import com.github.milkdrinkers.colorparser.ColorParser;
+import io.github.alathra.alathraskills.AlathraSkills;
 import io.github.alathra.alathraskills.utility.Cfg;
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
@@ -23,7 +24,7 @@ public class EasyPicking {
             player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, getDuration(skillLevel) * 20, 0));
             player.sendActionBar(ColorParser.of("<dark_grey>Easy Picking <green><bold>activated</bold></green></dark_grey>").build());
             // Notify when cooldown is over
-            Bukkit.getScheduler().runTaskLater(instance,
+            Bukkit.getScheduler().runTaskLater(AlathraSkills.getInstance(),
                 () -> player.sendActionBar(ColorParser.of("<dark_grey>Easy Picking <green><bold>available</bold></green></dark_grey>").build()), getCooldownTime(skillLevel) * 20
             );
         }
@@ -41,62 +42,30 @@ public class EasyPicking {
 
     // In seconds
     private static int getDuration(int skillLevel) {
-        switch (skillLevel) {
-            case 1 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.duration.l1");
-            }
-            case 2 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.duration.l2");
-            }
-            case 3 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.duration.l3");
-            }
-            case 4 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.duration.l4");
-            }
-            case 5 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.duration.l5");
-            }
-            case 6 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.duration.l6");
-            }
-            case 7 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.duration.l7");
-            }
-            default -> {
-                return 0;
-            }
-        }
+        return switch (skillLevel) {
+            case 1 -> Cfg.get().getInt("skills.mining.easyPickings.duration.l1");
+            case 2 -> Cfg.get().getInt("skills.mining.easyPickings.duration.l2");
+            case 3 -> Cfg.get().getInt("skills.mining.easyPickings.duration.l3");
+            case 4 -> Cfg.get().getInt("skills.mining.easyPickings.duration.l4");
+            case 5 -> Cfg.get().getInt("skills.mining.easyPickings.duration.l5");
+            case 6 -> Cfg.get().getInt("skills.mining.easyPickings.duration.l6");
+            case 7 -> Cfg.get().getInt("skills.mining.easyPickings.duration.l7");
+            default -> throw new IllegalStateException("Unexpected value: " + skillLevel);
+        };
     }
 
     // In seconds
     private static long getCooldownTime(int skillLevel) {
-        switch (skillLevel) {
-            case 1 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.cooldown.l1");
-            }
-            case 2 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.cooldown.l2");
-            }
-            case 3 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.cooldown.l3");
-            }
-            case 4 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.cooldown.l4");
-            }
-            case 5 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.cooldown.l5");
-            }
-            case 6 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.cooldown.l6");
-            }
-            case 7 -> {
-                return Cfg.get().getInt("skills.mining.easyPickings.cooldown.l7");
-            }
-            default -> {
-                return 0;
-            }
-        }
+        return switch (skillLevel) {
+            case 1 -> Cfg.get().getInt("skills.mining.easyPickings.cooldown.l1");
+            case 2 -> Cfg.get().getInt("skills.mining.easyPickings.cooldown.l2");
+            case 3 -> Cfg.get().getInt("skills.mining.easyPickings.cooldown.l3");
+            case 4 -> Cfg.get().getInt("skills.mining.easyPickings.cooldown.l4");
+            case 5 -> Cfg.get().getInt("skills.mining.easyPickings.cooldown.l5");
+            case 6 -> Cfg.get().getInt("skills.mining.easyPickings.cooldown.l6");
+            case 7 -> Cfg.get().getInt("skills.mining.easyPickings.cooldown.l7");
+            default -> throw new IllegalStateException("Unexpected value: " + skillLevel);
+        };
     }
 
 }
